2eddf0d6de7841ab8c61c4e55bc03c2df7f418aa
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2018-03-05 19:18:54 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcbasic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
