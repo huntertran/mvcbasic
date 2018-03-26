9651bc64cdbf4e521fd78dcd941ec79ca2c6330a
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2018-03-26 19:13:21 +0700

    phone model validation sample

--- Models/Phone.cs
+++ Models/Phone.cs
@@ -1,8 +1,12 @@
 namespace mvcbasic.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Phone
     {
         public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name cannot be more than 50 characters")]
         public string Name { get; set; }
     }
 }
